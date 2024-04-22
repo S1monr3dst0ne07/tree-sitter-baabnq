bedbd7f4703b624bf48f48b7f449a2e49998e50c
S1mon <jaforsi.kozak@gmail.com>
2024-04-22 21:07:20 +0200

    added highlights.scm

---
+++ queries/highlights.scm
@@ -0,0 +1,41 @@
+(var) @type
+(string) @string
+(number) @number
+(label) @function
+
+"put"     @keyword
+"input"   @keyword
+"print"   @keyword
+"putchr"  @keyword
+"lab"     @keyword
+"jump"    @keyword
+"sub"     @keyword
+"return"  @keyword
+"pull"    @keyword
+"push"    @keyword
+"new"     @keyword
+"free"    @keyword
+"static"  @keyword
+"use"     @include
+"asm"     @keyword
+
+
+"+"  @operator
+"-"  @operator
+"|"  @operator
+"&"  @operator
+"^"  @operator
+"<<" @operator
+">>" @operator
+"="  @operator
+"->" @operator
+"<-" @operator
+
+"==" @conditional
+"!=" @conditional
+">"  @conditional
+"<"  @conditional
+"~"  @conditional
+
+(eos) @delimiter
+(comment) @comment
