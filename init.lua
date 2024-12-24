4da546831a96de8feb5ddae459c79f608104de8d
S1mon <jaforsi.kozak@gmail.com>
2024-12-25 00:49:04 +0100

    added installer script fixed init.lua

--- init.lua
+++ init.lua
@@ -1,33 +1,4 @@
-local base_dir = vim.env.LUNARVIM_BASE_DIR
-  or (function()
-    local init_path = debug.getinfo(1, "S").source
-    return init_path:sub(2):match("(.*[/\\])"):sub(1, -2)
-  end)()
-
-if not vim.tbl_contains(vim.opt.rtp:get(), base_dir) then
-  vim.opt.rtp:prepend(base_dir)
-end
-
-require("lvim.bootstrap"):init(base_dir)
-
-require("lvim.config"):load()
-
-local plugins = require "lvim.plugins"
-
-require("lvim.plugin-loader").load { plugins, lvim.plugins }
-
-require("lvim.core.theme").setup()
-
-local Log = require "lvim.core.log"
-Log:debug "Starting LunarVim"
-
-local commands = require "lvim.core.commands"
-commands.load(commands.defaults)
-
-
-
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.baabnq = {
   install_info = {
     url = "~/tree-sitter-baabnq", -- local path or git repo
---
+++ install.sh
@@ -0,0 +1,3 @@
+cat init.lua >> ~/.config/lvim/config.lua
+mkdir -p ~/.config/nvim/queries/baabnq
+cp queries/highlights.scm ~/.config/nvim/queries/baabnq/
