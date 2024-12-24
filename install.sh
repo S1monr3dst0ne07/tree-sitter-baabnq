734482c219e4b3bdda05938893b184487d2a1dc3
S1mon <jaforsi.kozak@gmail.com>
2024-12-25 00:57:27 +0100

    added alternative nvim path in installer

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 cat init.lua >> ~/.config/lvim/config.lua
 mkdir -p ~/.config/nvim/queries/baabnq
 cp queries/highlights.scm ~/.config/nvim/queries/baabnq/
+mkdir -p ~/.local/share/nvim/queries/baabnq
+cp queries/highlights.scm ~/.local/share/nvim/queries/baabnq/
+
